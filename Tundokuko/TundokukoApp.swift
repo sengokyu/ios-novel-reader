0c7398c5ad60dc0dff27de5afb15abe684fbd705
sengokyu <sengokyu+gh@gmail.com>
2026-05-30 12:47:56 +0900

    Initial Commit

---
+++ Tundokuko/TundokukoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TundokukoApp.swift
+//  Tundokuko
+//
+//  Created by ziyi on R 8/05/30.
+//
+
+import SwiftUI
+
+@main
+struct TundokukoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
